2f73bb97b6f7cab803bf08689e5e4b5e4b7469b0
wild_wolf14 <1733779521@qq.com>
2022-12-14 21:24:46 +0800

    add bottom nav

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+function generate_main_tailer() {
+	var main_tailer = document.getElementById("main-tailer");
+	main_tailer.style =
+		"width: 100%;height: 50px;line-height: 50px;background-color:#e8e8e8;clear: both;text-align: center;position:fixed;bottom:0;";
+	main_tailer.innerHTML =
+		"<span style=\"margin-right: 20px;\">Copyright © 2022 Lytuph</span>";
+}
